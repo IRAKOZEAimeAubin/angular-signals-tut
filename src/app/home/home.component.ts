97e181e200833ffcec420133412ecb8125f7abdb
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2024-07-11 17:55:15 +0200

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  counter = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log('counter: ', this.counter());
+    });
+  }
+
+  increment() {
+    this.counter.update((val) => val + 1);
+  }
+}
